8b6752bace7b961d35743f23cbbb50c80188d199
armando <armandolopez0611@gmail.com>
2025-02-20 15:30:17 -0400

    Add: setting component

--- front-a/src/app/app.routes.constans.ts
+++ front-a/src/app/app.routes.constans.ts
@@ -3,4 +3,5 @@ export const LOGIN_ROUTE = 'login';
 export const PERSONAL_INFO_FORM_ROUTE = 'personal-info-form';
 export const SKILLS_FORM_ROUTE = 'skills-form';
 export const PROFILE_PAGE_ROUTE = 'profile';
-export const ADMIN_PAGE_ROUTE = 'admin'
+export const ADMIN_PAGE_ROUTE = 'admin';
+export const SETTING_PAGE_ROUTE = 'setting';
--- front-a/src/app/app.routes.ts
+++ front-a/src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { PersonalInfoComponent } from './components/forms/registerforms/personal
 import { SkillsComponent } from './components/forms/registerforms/skills/skills.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
-import { LOGIN_ROUTE, PERSONAL_INFO_FORM_ROUTE, SKILLS_FORM_ROUTE, PROFILE_PAGE_ROUTE, ADMIN_PAGE_ROUTE } from './app.routes.constans';
+import { LOGIN_ROUTE, PERSONAL_INFO_FORM_ROUTE, SKILLS_FORM_ROUTE, PROFILE_PAGE_ROUTE, ADMIN_PAGE_ROUTE, SETTING_PAGE_ROUTE } from './app.routes.constans';
 
 export const routes: Routes = [
     
@@ -13,5 +14,6 @@ export const routes: Routes = [
     {path: PERSONAL_INFO_FORM_ROUTE, component: PersonalInfoComponent},
     {path: SKILLS_FORM_ROUTE, component: SkillsComponent},
     {path: PROFILE_PAGE_ROUTE, component: ProfilePageComponent},
-    {path: ADMIN_PAGE_ROUTE, component: AdminPageComponent}
+    {path: ADMIN_PAGE_ROUTE, component: AdminPageComponent},
+    {path: SETTING_PAGE_ROUTE, component: SettingsPageComponent}
 ];
