0bd954ed88065f976dab1d41e898affda9d10bb1
armando <armandolopez0611@gmail.com>
2025-03-24 08:01:20 -0400

    change distribution in profesional information section

--- front-a/src/app/app.component.ts
+++ front-a/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegisterReactiveFormComponent } from './components/forms/registerforms/register-reactive-form/register-reactive-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterReactiveFormComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
