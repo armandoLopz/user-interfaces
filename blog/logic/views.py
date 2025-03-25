ce80ec8969ad95927f89b57eaadcae4b9d1669a8
armando <armandolopez0611@gmail.com>
2025-03-25 18:05:19 -0400

    add routes with query parameter

--- blog/logic/views.py
+++ blog/logic/views.py
@@ -24,22 +24,70 @@ class Address_view(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = Address_serializer
 
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user', None)
+        if user_id is not None:
+            
+            return Address.objects.filter(user__id=user_id)
+        else:
+            return super().get_queryset()
+
 class Work_experience_view(viewsets.ModelViewSet):
     queryset = Work_experience.objects.all()
     serializer_class = Work_experience_serializer
 
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user', None)
+        if user_id is not None:
+            
+            return Work_experience.objects.filter(user__id=user_id)
+        else:
+            return super().get_queryset()
+        
 class Education_view(viewsets.ModelViewSet):
     queryset = Education.objects.all()
     serializer_class = Education_serializer
 
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user', None)
+        if user_id is not None:
+            
+            return Education.objects.filter(user__id=user_id)
+        else:
+            return super().get_queryset()
+        
 class Languages_view(viewsets.ModelViewSet):
     queryset = Languages.objects.all()
     serializer_class = Languages_serializer
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user', None)
+        if user_id is not None:
+            
+            return Languages.objects.filter(user__id=user_id)
+        else:
+            return super().get_queryset()
 
 class Skills_view(viewsets.ModelViewSet):
     queryset = Skills.objects.all()
     serializer_class = Skills_serializer
 
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user', None)
+        if user_id is not None:
+            
+            return Skills.objects.filter(user__id=user_id)
+        else:
+            return super().get_queryset()
+
 class Competencies_view(viewsets.ModelViewSet):
     queryset = Competencies.objects.all()
     serializer_class = Competencies_serializer
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user', None)
+        if user_id is not None:
+            
+            return Competencies.objects.filter(user__id=user_id)
+        else:
+            return super().get_queryset()
