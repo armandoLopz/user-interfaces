89dfa913267db33c3678f223250093efd1b7be5f
armando <armandolopez0611@gmail.com>
2025-04-29 08:48:19 -0400

    add edit and delete in avatar component

--- front-a/src/app/components/avatar/avatar.component.ts
+++ front-a/src/app/components/avatar/avatar.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
+  standalone: true,
   imports: [],
   templateUrl: './avatar.component.html',
-  styleUrl: './avatar.component.css'
+  styleUrls: ['./avatar.component.css']
 })
 export class AvatarComponent {
+  
+  defaultImage = 'assets/profile-icon-design-free-vector.jpg';
+  userImage = signal<string | null>(null);
 
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+
+  triggerImageUpload() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onImageSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files?.length) return;
+
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.userImage.set(reader.result as string);
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  removeImage() {
+    if (confirm('Are you sure you want to remove your avatar?')) {
+      this.userImage.set(null);
+    }
+  }
 }
